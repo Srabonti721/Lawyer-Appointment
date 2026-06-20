302f19190f07e08bf10aac8a495527f33dead892
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-06-20 23:37:13 +0600

    banner data change used background image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import banner from '../assets/banner-img-1.png'
 const Banner = () => {
-    return (     
-<div className="hero mb-10 rounded-2xl">
-    <img className='rounded-xl' src={banner} alt="banner" />
-  <div className="hero-overlay"></div>
-  <div className="hero-content text-neutral-content text-center">
-    <div className="min-w-md">
-      <h1 className="mb-5 text-xl font-bold">It avoids subjective claims or exaggeration that might raise red flags legally</h1>
-      <p className="mb-5">
-   Our platform connects you with verified, experienced doctors across various specialties — all at your convenience. Whether it's a routine checkup or urgent consultation, book appointments in minutes and receive quality care you can trust.
-      </p>
+  return (
+    <div className="hero h-[500px] mb-10 rounded-xl"
+      style={{ backgroundImage:`url(${banner})`,}}>
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="min-w-md">
+          <h1 className="mb-5 text-xl font-bold">It avoids subjective claims or exaggeration that might raise red flags legally</h1>
+          <p className="mb-5">
+            Our platform connects you with verified, experienced doctors across various specialties — all at your convenience. Whether it's a routine checkup or urgent consultation, book appointments in minutes and receive quality care you can trust.
+          </p>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default Banner;
